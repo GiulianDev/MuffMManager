c97baa8095dcbd9744e1dde49f1502a69897f5f0
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2023-06-21 15:28:54 +0200

    change detail modal

--- MuffManager/src/app/COMPONENTS/detail-modal/detail-modal.component.ts
+++ MuffManager/src/app/COMPONENTS/detail-modal/detail-modal.component.ts
@@ -2,30 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Lego } from 'src/app/MODELS/CLASSES/Lego';
+import { CurrencyComponent } from '../currency/currency.component';
 
 @Component({
   selector: 'app-detail-modal',
   templateUrl: './detail-modal.component.html',
   styleUrls: ['./detail-modal.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, CurrencyComponent]
 
 })
-export class DetailModalComponent  implements OnInit {
+export class DetailModalComponent {
 
-  public item: Lego | undefined;
+  public item: Lego = this.navParams.get('data');
 
   constructor(
     private modalController: ModalController,
     private navParams: NavParams,
-    ) {
-    }
+    ) {}
     
-    ngOnInit() {
-    this.item = this.navParams.get('data');
-    console.log("item:", this.item);
-
-  }
+  // ngOnInit() {
+  //   this.item = this.navParams.get('data');
+  //   console.log("item:", this.item);
+  // }
 
   dismissModal() {
     this.modalController.dismiss();
